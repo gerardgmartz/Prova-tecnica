0e3fac458c68d6b16c38af0b2f3580bcfaccb1e5
gerardgmartz <gerardgmartz@gmail.com>
2022-05-20 08:49:21 +0200

    react2

--- prova-tecnica/src/App.js
+++ prova-tecnica/src/App.js
@@ -1,24 +1,31 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DataTable from 'react-data-table-component';
+import 'react-data-table-component-extensions/dist/index.css';
+import { columns, data } from './Data.js';
+
+
 
 function App() {
+  const tableData = {
+    columns,
+    data,
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataTableExtensions
+      {...tableData}
+    >
+      <DataTable
+        noHeader
+        defaultSortField="id"
+        defaultSortAsc={false}
+        pagination
+        highlightOnHover
+      />
+    </DataTableExtensions>
   );
 }
 
